dad7e349ba454c4a2c4817181d382b5d6305b164
Jose Mendez <pepe_jmendez23@hotmail.com>
2014-10-20 16:48:56 -0400

    added some improvements

--- defaultController.js
+++ defaultController.js
@@ -1,71 +1,72 @@
-﻿angular.module("default", ["ngCrud"]).
+﻿angular.module("default", ["ngCrud", "firebase"])
 
-controller("mainWraperController", function ($scope, crudServiceApi, configCrudService) {
+ .controller("mainWraperController", function ($scope, crudServiceApi, configCrudService) {
+   
 
-    var servicesContract = {
-        getAllItems: crudServiceApi.getAllItems,
-        getItem: crudServiceApi.getItem,
-        saveItem: crudServiceApi.saveItem,
-        modifyItem: crudServiceApi.modifyItem,
-        deleteItem: crudServiceApi.deleteItem
-    };
+     var servicesContract = {
+         getAllItems: crudServiceApi.getAllItems,
+         getItem: crudServiceApi.getItem,
+         saveItem: crudServiceApi.saveItem,
+         modifyItem: crudServiceApi.modifyItem,
+         deleteItem: crudServiceApi.deleteItem
+     };
 
-    var dataContract = {
-        Id: ["Id", "false", "int"],
+     var dataContract = {
+         Id: ["$id", "false", "string"],
 
-        Website: {
-            link: {
-                text:["Name", "true", "string"],
-                url: ["Site", "true", "string"]
-            }
-        },
+         site: {
+             link: {
+                 text: ["name", "true", "string"],
+                 url: ["site", "true", "string"]
+             }
+         },
 
-        Description : ["Description", "true", "text"]
-    }
+         description: ["description", "true", "text"]
+     }
 
-    //data for the crud
-    var data = {
-        ngCrud: "firstCrudIdentifier",
-        servicesContract: servicesContract,
-        dataContract: dataContract,
-        panelTitle: "",
-        options: {}
-    }
+     //data for the crud
+     var data = {
+         ngCrud: "firstCrudIdentifier",
+         servicesContract: servicesContract,
+         dataContract: dataContract,
+         panelTitle: "",
+         options: {}
+     }
 
-    //configCrudService.setContracts(data);
-
-
-    //options for the droppable directive
-    var dragAndDropOptions = {
-        crudKey: {
-            data: data,
-            tag: "div",
-            attr: {
-                "ng-crud": "firstCrudIdentifier"
-            },
-            cleanerSelectorID:"cleanContent1",
-            onDropCallback: function () {
-                console.log("ng-crud drop callback called");
-            }
-        },
-
-        customDir: {
-            data: data,
-            tag: "delete-Icon",
-            cleanerSelectorID: "cleanContent1",
-            onDropCallback: function () {
-                console.log("delete-Icon drop callback called");
-            }
-        }
-
-    }
+     //configCrudService.setContracts(data);
 
 
-    $scope.dropOptions = {
-        activeClass: "ui-state-default",
-        hoverClass: "ui-state-hover",
-        accept: ":not(.ui-sortable-helper)"
-    }
+     //options for the droppable directive
+     var dragAndDropOptions = {
+         crudKey: {
+             data: data,
+             tag: "div",
+             attr: {
+                 "ng-crud": "firstCrudIdentifier"
+             },
+             cleanerSelectorID: "cleanContent1",
+             onDropCallback: function () {
+                 console.log("ng-crud drop callback called");
+             }
+         },
 
-    $scope.options = dragAndDropOptions;
-});
+         customDir: {
+             data: data,
+             tag: "delete-Icon",
+             cleanerSelectorID: "cleanContent1",
+             onDropCallback: function () {
+                 console.log("delete-Icon drop callback called");
+             }
+         }
+
+     }
+
+
+     $scope.dropOptions = {
+         activeClass: "ui-state-default",
+         hoverClass: "ui-state-hover",
+         accept: ":not(.ui-sortable-helper)"
+     }
+
+     $scope.options = dragAndDropOptions;
+ })
--- defaultServices.js
+++ defaultServices.js
@@ -1,64 +1,74 @@
 ﻿angular.module("default")
 
+
 .value('fbURL', 'https://angularjs-projects.firebaseio.com/')
 
-.factory('firebaseObj', function ($firebase, fbURL) {
-    //return $firebase(new Firebase(fbURL)).$asArray();
-    return null
+    .service('parceIdFirebase', function () {
+        this.parse  = function (obj) {
+            if (obj == null || obj.length == 0)
+                return;
+            var aux = [];
+            _.each(obj, function (item) {
+                if (item != null && !item.Id) {
+                    item.Id = item.$id;
+                    aux.push(item);
+                } else {
+                    aux.push(item);
+                }
+            });
+            return aux;
+        }
+    })
+
+.factory('FirebaseObj', function ($firebase, fbURL) {
+    return $firebase(new Firebase(fbURL)).$asArray();
+    // return null
 })
 
-.service("crudServiceApi", [ '$http', '$q', '$location', 'firebaseObj', 'ajaxLoadingService', function ($http, $q, $location, firebaseObj, ajaxLoadingService) {
-        var data = {
-            id: 0
-        }
+.service("crudServiceApi", function ($http, $q, $location, ajaxLoadingService, FirebaseObj, parceIdFirebase) {
+    var data = {
+        id: 0
+    }
 
-        this.getAllItems = function () {
-            var dfr = $q.defer();
-            $http.get("api/data/getAllItems").success(function (response) {
-                dfr.resolve(response);
-            });
+    this.getAllItems = function () {
+        var dfr = $q.defer();
+        dfr.resolve(parceIdFirebase.parse(FirebaseObj));
+        return dfr.promise;
+    };
 
-            return dfr.promise;
-        };
+    this.getItem = function (id) {
+        var dfr = $q.defer();
 
-        this.getItem = function (id) {
-            var dfr = $q.defer();
-            $http.get("api/data/getItem/" + id).success(function (response) {
-                dfr.resolve(response);
-            });
+        var fbid = FirebaseObj.$indexFor(id);
+        dfr.resolve(FirebaseObj[fbid]);
+        return dfr.promise;
+    };
 
-            return dfr.promise;
-        };
+    this.saveItem = function (dataTodoData) {
+        var dfr = $q.defer();
+        
+        dfr.resolve(FirebaseObj.$add(dataTodoData));
+        return dfr.promise;
+        
+    };
 
-        this.saveItem = function (dataTodoData) {
-            var dfr = $q.defer();
-            $http.post("api/data/saveItem", dataTodoData).success(function (response) {
-                dfr.resolve(response);
-            });
+    this.modifyItem = function (dataTodoData) {
+        var dfr = $q.defer();
+        $http.post("api/data/modifyItem", dataTodoData).success(function (response) {
+            dfr.resolve(response);
+        });
 
-            return dfr.promise;
-        };
+        return dfr.promise;
+    };
 
-        this.modifyItem = function (dataTodoData) {
-            var dfr = $q.defer();
-            $http.post("api/data/modifyItem", dataTodoData).success(function (response) {
-                dfr.resolve(response);
-            });
+    this.deleteItem = function (id) {
+        var dfr = $q.defer();
+        dfr.resolve(FirebaseObj.$remove(id));
+        return dfr.promise;
+    };
 
-            return dfr.promise;
-        };
+    this.refreshView = function () {
+        return this.getAllItems();
+    }
 
-        this.deleteItem = function (id) {
-            var dfr = $q.defer();
-            $http.delete("api/data/deleteItem/"+ id).success(function (response) {
-                dfr.resolve(response);
-            });
-
-            return dfr.promise;
-        };
-
-        this.refreshView = function () {
-           return this.getAllItems();
-        }
-
-    }]);
+})
